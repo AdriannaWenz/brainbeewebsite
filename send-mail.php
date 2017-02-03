92342979f5fe81e5b1971edb7e63720710393310
AdriannaWenz <adrianna_wenz@brown.edu>
2017-02-03 13:58:15 -0500

    Update send-mail.php

--- send-mail.php
+++ send-mail.php
@@ -15,7 +15,7 @@ $message=$_POST['message'];
     $body .= "Message: " . $message . "\n"; 
 
     //replace with your email
-    mail("test@email.com","New email",$body); 
+    mail("admin@brownbrainbee.org","New email",$body); 
 
   
 ?>
